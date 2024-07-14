d9b255789d77e6c6afd3ee648b56bb98ec8f18a8
Ilya Lyasin <lyasin.i@mail.ru>
2024-07-14 13:07:18 +0300

    Initial Commit

---
+++ MyCyprusApp/MyCyprusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCyprusAppApp.swift
+//  MyCyprusApp
+//
+//  Created by Ilia Liasin on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyCyprusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
